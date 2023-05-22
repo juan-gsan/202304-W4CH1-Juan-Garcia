e90febe4f26e96e195ee81fce0bd9f07f6b4f88f
JuanG <juan.gsanchez13@gmail.com>
2023-05-22 18:31:28 +0200

    Add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./components/Button";
 import { Header } from "./components/Header";
 
 function App() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <Button></Button>
+    </>
+  );
 }
 
 export default App;
